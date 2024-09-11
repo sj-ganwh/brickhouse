5ead90157131b8d062934239db79c340da81c36a
sj-ganwh <155623319+sj-ganwh@users.noreply.github.com>
2024-09-11 02:50:34 +0000

    add cipher decode

---
+++ src/main/java/brickhouse/udf/sketch/CipherDecode.java
@@ -0,0 +1,46 @@
+package brickhouse.udf.sketch;
+/**
+ * Copyright 2012 Klout, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * Calculate Cipher of the string
+ */
+@Description(name="cipher_decode",
+value = "_FUNC_(a,b) - Decode a with secret b " 
+)
+public final class CipherDecode extends UDF {
+
+    public String evaluate(String encodedContent, String secretKey) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedContent);
+
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
